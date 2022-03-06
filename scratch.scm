72c7b25d9e03bccc6a565655d8f3ebaf7294b919
tomh <irontom@disroot.org>
2022-03-06 11:50:52 -0500

    Halfway through chapter 9

--- scratch.scm
+++ scratch.scm
@@ -1216,3 +1216,235 @@
     (cons sum (cons product newl))))
 
 (evens-only*&co '((9 1 2 8) 3 10 ((9 9) 7 6) 2) the-last-friend)
+
+(define looking
+  (lambda (a lat)
+    (keep-looking a (pick 1 lat) lat)))
+
+
+(define keep-looking
+  (lambda (a sorn lat)
+    (cond
+     ((number? sorn)
+      (keep-looking a (pick sorn lat) lat))
+     (else (equal? sorn a)))))
+
+(looking 'caviar '(6 2 4 caviar 5 7 3))
+(looking 'caviar '(6 2 grits caviar 5 7 3))
+
+(define eternity
+  (lambda (x)
+    (eternity x)))
+
+
+(define shift
+  (lambda (pair)
+    (build (first (first pair))
+           (build (second (first pair))
+                  (second pair)))))
+
+
+(define align
+  (lambda (pora)
+    (cond
+     ((atom? pora) pora)
+     ((a-pair? (first pora))
+      (align (shift pora)))
+     (else
+      (build (first pora)
+             (align (second pora)))))))
+
+(define length*
+  (lambda (pora)
+    (cond
+    ((atom? pora) 1)
+    (else
+     (+ (length* (first pora))
+        (length* (second pora)))))))
+
+(length* '((a (b c)) ((d e) (f (g h)))))
+
+(define weight*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (+ (* (weight* (first pora)) 2)
+         (weight* (second pora)))))))
+
+(define shuffle
+  (lambda (pora)
+    (cond
+     ((atom? pora) pora)
+     ((a-pair? (first pora))
+       (shuffle (revpair pora)))
+      (else
+       (build
+        (first pora)
+        (shuffle (second pora)))))))
+
+
+(define build
+  (lambda (s1 s2)
+    (cond
+     (else
+      (cons s1 (cons s2 (quote ())))))))
+
+(define C
+  (lambda (n)
+    (cond
+     ((one? n) 1)
+     (else
+      (cond
+       ((even? n) (C (/ n 2)))
+       (else
+        (C (add1 (* 3 n)))))))))
+
+(define A
+  (lambda (n m)
+    (cond
+     ((zero? n) (add1 m))
+     ((zero? m) (A (sub1 n) 1))
+     (else
+      (A (sub1 n)
+         (A n (sub1 m)))))))
+
+(((lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 (length (cdr l)))))))
+  eternity) '())
+
+
+((lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 (length (cdr l))))))) eternity)
+
+((lambda (f)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 (f (cdr l)))))))
+ ((lambda (g)
+    (lambda (l)
+      (cond
+       ((null? l) 0)
+       (else (add1 (length (cdr l)))))))
+ eternity))
+
+((lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 length (cdr l))))))
+ ((lambda (length)
+    (lambda (l)
+      (cond
+       ((null? l) 0)
+       (else (add1 length (cdr l))))))
+  ((lambda (length)
+     (lambda (l)
+       (cond
+        ((null? l) 0)
+        (else (add1 length (cdr l)))))) eternity)))
+
+((lambda (mk-length)
+   (mk-length eternity))
+ (lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 (length (cdr l))))))))
+
+((lambda (mk-length)
+   (mk-length
+    (mk-length
+     (mk-length
+      (mk-length eternity)))))
+ (lambda (length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1 (length (cdr l))))))))
+
+(((lambda (mk-length)
+   (mk-length mk-length))
+ (lambda (mk-length)
+   (lambda (l)
+     (cond
+      ((null? l) 0)
+      (else (add1
+             ((mk-length eternity) (cdr l)))))))) '(1))
+
+(define looking
+  (lambda (a lat)
+    (keep-looking a (pick 1 lat) lat)))
+
+(define keep-looking
+  (lambda (a sorn lat)
+    (cond
+     ((number? sorn)
+      (keep-looking a (pick sorn lat) lat))
+     (else (eq? sorn a)))))
+
+;; (looking 'caviar '(7 1 2 caviar 5 6 3))
+
+(pick 4 '(7 1 2 caviar 5 6 3))
+
+(define eternity
+  (lambda (x)
+    (eternity x)))
+
+(define shift
+  ;; Take a pair and
+  ;; 1. extract the first atom from the first pair
+  ;; 2. add the second atom from the first pair to the second pair
+  (lambda (pair)
+    (build
+     (first (first pair))
+     (build (second (first pair))
+            (second pair)))))
+
+
+(define length*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (+ (length* (first pora))
+         (length* (second pora)))))))
+
+(length* '((a b) (c (d (e f)))))
+
+(define weight*
+  (lambda (pora)
+    (cond
+     ((atom? pora) 1)
+     (else
+      (+ (* (weight* (first pora)) 2)
+         (weight* (second pora)))))))
+
+
+(weight* '((a b) (c d)))
+
+(length* '((a b) (c (d (e f)))))
+
+
+(define shuffle
+  (lambda (pora)
+    (cond
+     ((atom? pora) pora)
+     ((a-pair? (first pora))
+      (shuffle (revpair pora)))
+     (else
+      (build
+       (first pora)
+       (shuffle (second pora)))))))
+
+(shuffle '((a b) c))
+
+(define will-stop?
+  ())
